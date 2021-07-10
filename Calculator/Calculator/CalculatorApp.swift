ab48a56fa86a8a659bf456b3e19348868982318a
Luis Genesius <luis.huang321@gmail.com>
2021-07-10 10:10:25 +0700

    create xcode project with swiftui framework

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Luis Genesius on 10/07/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
