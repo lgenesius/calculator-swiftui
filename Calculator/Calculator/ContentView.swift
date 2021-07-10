a74e9c1656d80d3124595578a61c55140c2e9152
Luis Genesius <luis.huang321@gmail.com>
2021-07-10 10:41:56 +0700

    add calc button enum and init button display button

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -7,8 +7,38 @@
 
 import SwiftUI
 
+enum CalcButton: String {
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case zero = "0"
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "*"
+    case equal = "="
+    case clear = "AC"
+    case decimal = "."
+    case percent = "%"
+    case negative = "-/+"
+}
+
 struct ContentView: View {
     var body: some View {
+        
+        let buttons: [[CalcButton]] = [
+            [.clear, .negative, .percent, .divide],
+            [.seven, .eight, .nine, .multiply],
+            [.four, .five, .six, .subtract],
+            [.one, .two, .three, .add],
+            [.zero, .decimal, .equal],
+        ]
         
         ZStack {
             Color.black
@@ -26,6 +56,22 @@ struct ContentView: View {
                 .padding()
                 
                 // Buttons Display
+                ForEach(buttons, id: \.self) { row in
+                    HStack {
+                        ForEach(row, id: \.self) { item in
+                            Button(action: {
+                                
+                            }, label: {
+                                Text(item.rawValue)
+                                    .font(.system(size: 32))
+                                    .frame(width: 70, height: 70)
+                                    .background(Color.orange)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(35)
+                            })
+                        }
+                    }
+                }
             }
         }
     }
